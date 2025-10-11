9896fd45abe6357b58029676b5bcd8c1c08fd887
enriicola <enriicola@proton.me>
2025-10-11 17:45:07 +0200

    Add QPushButton to main application window in Counter App

--- widgets-signals/src/main.cpp
+++ widgets-signals/src/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QWidget>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
@@ -7,6 +8,13 @@ int main(int argc, char *argv[])
 
     QWidget window;
     window.setWindowTitle("Counter App");
+    window.resize(200, 100);
+
+    // Create a simple button
+    QPushButton *button = new QPushButton("Click me!", &window);
+    button->resize(100, 30);
+    button->move(50, 35);  // Position it in the center
+
     window.show();
 
     return app.exec();
